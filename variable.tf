7101d87d0890fd53986f0234bd792ffb6267d2af
Akshay-Vallam <149242737+Akshay-Vallam@users.noreply.github.com>
2023-11-25 00:32:21 +1100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "The AWS region in which to create the bucket"
+  default     = "ap-southeast-2" // Change this to your preferred default region
+}
+
+variable "repo_name" {
+  description = "The name of the repository to be created"
+  default     = "sample-ecr" // Replace with your desired repository name
+}
