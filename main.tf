a511676f4a83e2f64a45ada105391ccac6127282
Akshay-Vallam <149242737+Akshay-Vallam@users.noreply.github.com>
2023-11-25 00:41:49 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_ecr_repository" "my_repo" {
-  bucket = var.repo_name
+resource "aws_ecr_repository" "repository" {
+  name = var.repo_name
 }
